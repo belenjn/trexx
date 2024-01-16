a63565cca73490b764e8592b967b633b04dec4ea
Belen Jaraba <belen.develop@gmail.com>
2024-01-16 14:17:40 +0100

    Searcher with resposive

--- src/components/Searcher/Searcher.tsx
+++ src/components/Searcher/Searcher.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Searcher.css";
 import searchIcon from "../../assets/search.png";
+import { strings } from "../../utils/strings";
 
 export const Searcher = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleInput = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <div className="searcher-container">
-      <div className="searcher-input-container">
-        <img className="search-icon" src={searchIcon} alt="Search Icon" />
-        <input type="text" placeholder="Search artists:" />
-      </div>
+    <div className={`searcher-container ${expanded ? "expanded" : ""}`}>
+      <img
+        className={`search-icon ${expanded ? "hidden" : ""}`}
+        src={searchIcon}
+        alt="Search Icon"
+        onClick={toggleInput}
+      />
+      <input
+        type="text"
+        placeholder={`${strings.searcher.placeholder}`}
+        className={expanded ? "expanded" : ""}
+      />
     </div>
   );
 };
