56ac9cfeb62bd1aed22507917a775ea7de5a2bc9
Belen Jaraba <belen.develop@gmail.com>
2024-01-16 15:51:57 +0100

    Searcher function used

--- src/components/Searcher/Searcher.tsx
+++ src/components/Searcher/Searcher.tsx
@@ -2,13 +2,33 @@ import React, { useState } from "react";
 import "./Searcher.css";
 import searchIcon from "../../assets/search.png";
 import { strings } from "../../utils/strings";
+import { fetchData } from "../../utils/fetchData";
+import { useData } from "../../context/DataContext";
 
 export const Searcher = () => {
   const [expanded, setExpanded] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const { state, dispatch } = useData();
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetchData(searchTerm, "search");
+      dispatch({ type: "SET_DATA", payload: response?.data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const toggleInput = () => {
     setExpanded(!expanded);
   };
+
+  const handleKeyDown = (e: { key: string }) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+  console.log(state.data?.data, "estado");
 
   return (
     <div className={`searcher-container ${expanded ? "expanded" : ""}`}>
@@ -22,6 +42,8 @@ export const Searcher = () => {
         type="text"
         placeholder={`${strings.searcher.placeholder}`}
         className={expanded ? "expanded" : ""}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
     </div>
   );
