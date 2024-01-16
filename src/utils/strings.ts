c5da4b34a7a110c0424cd209c6c60f895fb4eb3e
Belen Jaraba <belen.develop@gmail.com>
2024-01-16 14:17:16 +0100

    strings added

---
+++ src/utils/strings.ts
@@ -0,0 +1,11 @@
+export const strings = {
+    menuItems: {
+        home: 'Home',
+        discover: 'Discover',
+        recents: 'Recents',
+        library: 'Library'
+    },
+    searcher: {
+        placeholder: 'Search artists'
+    }
+}
