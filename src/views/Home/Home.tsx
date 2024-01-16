ffedc71c3741926a0392537a97ff808550c7867c
Belen Jaraba <belen.develop@gmail.com>
2024-01-16 15:52:08 +0100

    New styles for Home

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import "./Home.css";
 import { Navbar } from "../../components/Navbar/Navbar";
 
+//TODO: revisar todos los strings, incluso de errores para pasarlos a strings.ts
+
 export const Home = () => {
   return (
     <div className="container">
       <Navbar />
+      <div className="home-container">
+        <label className="home-search-label">Search results for:</label>
+      </div>
     </div>
   );
 };
