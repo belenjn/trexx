7311e3b810477aca8d307d08e6906ef6f2f3371d
Belen Jaraba <belen.develop@gmail.com>
2024-01-17 12:26:03 +0100

    Home edited and styled

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,15 +1,42 @@
-import React from "react";
+import { useState } from "react";
 import "./Home.css";
 import { Navbar } from "../../components/Navbar/Navbar";
+import { useData } from "../../context/DataContext";
+import { Track } from "../../types/Track";
+import { GridCard } from "../../components/GridCard/GridCard";
+import { NoResults } from "../NoResults/NoResults";
 
-//TODO: revisar todos los strings, incluso de errores para pasarlos a strings.ts
+//TODO: Al hacer click en cualquiera de las canciones, abriremos una capa con el reproductor y reproduciremos la muestra de 30 segundos que nos devuelva la petición con los controles que se muestren en el diseño. Al pinchar en otra canción, la información del reproductor deberá cambiar.
+//TODO: repasar strings en archivo por todos los cmponentes
+//TODO: tests
 
 export const Home = () => {
+  const { state } = useData();
+  const tracks = state.data?.data.filter((artist) => artist.type === "track");
+
+  const [selectedTrack, setSelectedTrack] = useState<Track | null>(null);
+
+  const handleTrackClick = (track: Track) => {
+    setSelectedTrack(track);
+    // Lógica para abrir la capa del reproductor y reproducir la muestra de 30 segundos
+  };
+
   return (
     <div className="container">
       <Navbar />
       <div className="home-container">
-        <label className="home-search-label">Search results for:</label>
+        <label className="home-search-label">
+          Search results for: <b>{state.data?.data[1].artist.name}</b>
+        </label>
+        <div className={tracks ? "grid-container" : ""}>
+          {tracks ? (
+            tracks.map((track, index) => (
+              <GridCard key={index} track={track} onClick={handleTrackClick} />
+            ))
+          ) : (
+            <NoResults />
+          )}
+        </div>
       </div>
     </div>
   );
