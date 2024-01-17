f6d9feb39bfc243daf66b85df1c9dcd2d24f2fc3
Belen Jaraba <belen.develop@gmail.com>
2024-01-17 13:57:12 +0100

    MusicPlayer logic added

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Home.css";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { useData } from "../../context/DataContext";
@@ -6,6 +6,7 @@ import { Track } from "../../types/Track";
 import { GridCard } from "../../components/GridCard/GridCard";
 import { NoResults } from "../NoResults/NoResults";
 import { strings } from "../../utils/strings";
+import MusicPlayer from "../../components/MusicPlayer/MusicPlayer";
 
 //TODO: Al hacer click en cualquiera de las canciones, abriremos una capa con el reproductor y reproduciremos la muestra de 30 segundos que nos devuelva la petición con los controles que se muestren en el diseño. Al pinchar en otra canción, la información del reproductor deberá cambiar.
 //TODO: tests
@@ -15,11 +16,18 @@ export const Home = () => {
   const tracks = state.data?.data.filter((artist) => artist.type === "track");
 
   const [selectedTrack, setSelectedTrack] = useState<Track | null>(null);
+  const [isMusicPlayerOpen, setIsMusicPlayerOpen] = useState(false);
+  const [track, setTrack] = useState<Track | null>(null);
 
   const handleTrackClick = (track: Track) => {
     setSelectedTrack(track);
-    // Lógica para abrir la capa del reproductor y reproducir la muestra de 30 segundos
+    setIsMusicPlayerOpen(true);
+    setTrack(track);
   };
+
+  useEffect(() => {
+    setIsMusicPlayerOpen(false);
+  }, [state.data]);
 
   return (
     <div className="container">
@@ -39,6 +47,7 @@ export const Home = () => {
           )}
         </div>
       </div>
+      {track && isMusicPlayerOpen && <MusicPlayer track={track} />}
     </div>
   );
 };
